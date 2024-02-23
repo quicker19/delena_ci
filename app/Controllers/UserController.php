e3a26e4da31ffe2b496a66404548211058c34b2b
Christian Del <delenaxian@gmail.com>
2024-02-23 16:04:39 +0800

    first commit

---
+++ app/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class UserController extends BaseController
+{
+    public function index(): string
+    {
+        return view('welcome_message');
+    }
+
+    public function addUser() {
+        return view('user/add');
+    }
+}
---
+++ app/Views/user/add.php
@@ -0,0 +1,7 @@
+<?=$this->extend('layout/main')?>
+
+<?=$this->section('content')?>
+
+<h1>This is User Add Page.</h1>
+
+<?=$this->endSection('content')?>
